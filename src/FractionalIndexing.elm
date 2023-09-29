f4476ad1cd43ce73f267f4ca4f8407dac485455b
rlpt <richard.targett@gmail.com>
2023-09-29 10:23:45 +0100

    maybe this will work

--- src/FractionalIndexing.elm
+++ src/FractionalIndexing.elm
@@ -192,22 +192,72 @@ incrementInteger x =
         [] ->
             Nothing
 
-        head :: digs ->
+        head :: originalDigits ->
             let
-                _ =
-                    ""
+                ( carry, digits ) =
+                    inc1 originalDigits
             in
-            Just ""
+            if carry then
+                if head == 'Z' then
+                    Just (String.fromList [ 'a', getDigitAt 0 ])
+
+                else if head == 'z' then
+                    -- we have run out of integers
+                    Nothing
+
+                else
+                    let
+                        nextHeadDigit : Char
+                        nextHeadDigit =
+                            Char.toCode head + 1 |> Char.fromCode
+
+                        finalDigits =
+                            if nextHeadDigit > 'a' then
+                                digits ++ [ firstChar ]
+
+                            else
+                                List.take (List.length digits - 1) digits
+                    in
+                    Just (String.fromList (nextHeadDigit :: finalDigits))
+
+            else
+                Just (String.fromList (head :: digits))
 
 
 inc1 : List Char -> ( Bool, List Char )
 inc1 digs =
     let
         acc : Char -> ( Bool, List Char ) -> ( Bool, List Char )
-        acc dig prev =
-            prev
+        acc dig ( carry, prevDigs ) =
+            let
+                nextDigitIdx =
+                    String.indexes (String.fromChar dig) base62Digits
+                        |> List.head
+                        |> Maybe.map (\idx -> idx + 1)
+                        -- use -1 as default as it will fail on following checks
+                        |> Maybe.withDefault -1
+            in
+            if nextDigitIdx == String.length base62Digits then
+                --  nextDigitIndex is out of range, so we want to carry and continue on to next dig
+                ( True, firstChar :: prevDigs )
+
+            else
+                -- nextDigitIndex is still in range, so we can inc with carry
+                let
+                    incChar =
+                        getAt nextDigitIdx (String.toList base62Digits)
+                            -- use non standard char is totem to show this has failed, but it never should
+                            |> Maybe.withDefault '-'
+                in
+                ( False, incChar :: prevDigs )
     in
-    List.foldr acc ( True, [] ) digs
+    List.foldr acc ( True, digs ) digs
+
+
+getDigitAt : Int -> Char
+getDigitAt idx =
+    -- use non standard char '-' as totem to show this has failed
+    getAt idx (String.toList base62Digits) |> Maybe.withDefault '-'
 
 
 findCommonStringPrefix : String -> String -> String
@@ -261,8 +311,12 @@ parseIdx maybeIdxStr =
             Ok Blank
 
 
+firstChar =
+    '0'
+
+
 zero =
-    "0"
+    String.fromChar firstChar
 
 
 end =
