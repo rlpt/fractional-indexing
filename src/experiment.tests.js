da34a89ce906c38155cc455b6b8143bd2fc53f72
rlpt <richard.targett@gmail.com>
2023-10-11 07:08:38 +0100

    rejig js tests

--- src/experiment.tests.js
+++ src/experiment.tests.js
@@ -1,5 +1,7 @@
 const fi = require("./expirement.js");
 
+console.log(fi);
+
 describe("experiment", () => {
     // test("getIntegerLength", () => {
     //     try {
@@ -31,9 +33,34 @@ describe("experiment", () => {
 
     const BASE_62_DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    test("incrementInteger", () => {
-        console.log(fi.incrementInteger("bzz", fi.BASE_62_DIGITS)) 
+    /**
+     * @param {string | null} a
+     * @param {string | null} b
+     * @param {number} n
+     * @param {string} exp
+     */
+    function testN(a, b, n, exp) {
+        /** @type {string} */
+        let act;
+        try {
+            act = fi.generateNKeysBetween(a, b, n, BASE_62_DIGITS).join(" ");
+        } catch (exp) {
+            act = exp.message;
+        }
+
+        console.assert(exp == act, `${exp} == ${act}`);
+    }
+
+    test("nb", () => {
+        // testN("a4", null, 10, "a5 a6 a7 a8 a9 b00 b01 b02 b03 b04");
     })
+
+    // testN(null, null, 5, "a0 a1 a2 a3 a4");
+    // testN("a4", null, 10, "a5 a6 a7 a8 a9 b00 b01 b02 b03 b04");
+
+    // test("incrementInteger", () => {
+    //     console.log(fi.incrementInteger("bzz", fi.BASE_62_DIGITS)) 
+    // })
     
 
     // test("validateOrderKey", () => {
